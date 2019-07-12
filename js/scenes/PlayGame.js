ea6dab2a18869dbbad8bf273762f953e0e1b2ada
joeparrilla <joeparrilla@protonmail.com>
2019-07-12 15:13:13 -0400

    enemy work

--- js/scenes/PlayGame.js
+++ js/scenes/PlayGame.js
@@ -13,11 +13,11 @@ export class PlayGame extends Phaser.Scene {
     create() {
 
         this.playerDirection = 'up'
-        this.enemiesHeld = 0;
-        this.maxEnemiesHeld = 2;
+        this.maxEnemiesHeld  = 2;
+        this.enemiesHeld     = [];
 
         //text
-        this.heldText = this.add.text(16, 16, `held: ${this.enemiesHeld}`, { fontSize: '32px', fill: '#FFF' });
+        this.heldText = this.add.text(16, 16, `held: ${this.enemiesHeld.length}`, { fontSize: '32px', fill: '#FFF' });
 
         let cursors = this.input.keyboard.createCursorKeys(),
             player  = this.physics.add.sprite(game.canvas.width / 2, game.canvas.height / 2, 'player'),
@@ -29,53 +29,75 @@ export class PlayGame extends Phaser.Scene {
 
         this.cursors = cursors;
         this.player  = player;
-        this.rope = this.physics.add.sprite(this.player.x, this.player.y, 'rope').setActive(false).setVisible(false);
+        this.rope    = this.physics.add.sprite(this.player.x, this.player.y, 'rope').setActive(false).setVisible(false);
         this.rope.body.setEnable(false);
 
         this.enemies = this.physics.add.group({
-            key: 'enemy',
-            repeat: 20,
-            setXY: { x: Math.random() * 400, y:  Math.random() * 400, stepX: 45 }
+            key               : 'enemy',
+            repeat            : 5,
+            collideWorldBounds: true,
+            immovable         : true
         });
 
-        this.enemies.children.iterate((enemy) => {
-            enemy.setCollideWorldBounds(true);
-            enemy.setImmovable(true);
-        });
+        this.enemies.children.iterate((child) => {
+            child.setData({ roped: false, dropped: false });
+            child.setPosition(Math.floor(Math.random() * 401), Math.floor(Math.random() * 401));
+        })
 
+        //a default state in case the player uses the rope before making any movements. Once the player moves at least once, this is not needed
+        this.rope.setPosition(this.player.x, this.player.y - 40);
+        this.rope.setAngle(90);
+
+        /* 
+         * On Space press, we activate the rope sprite and then begin a .3 second timer. At the end of the timer, the rope is deactivated. This
+         * is to create the lasso effect. The rope can only interact with objects while it is active
+        */
         this.input.keyboard.on('keydown-SPACE', () => {
-            this.rope.active = true;
-            this.rope.visible = true;
+            this.rope.active      = true;
+            this.rope.visible     = true;
             this.rope.body.enable = true;
-            this.time.delayedCall(300, () => { 
-                this.rope.active = false;
-                this.rope.visible = false;
+            this.time.delayedCall(300, () => {
+                this.rope.active      = false;
+                this.rope.visible     = false;
                 this.rope.body.enable = false;
             }, [], this)
         });
 
+        /* 
+        On D press, if we have any carried enemies, we drop them and activate their physics
+        */
         this.input.keyboard.on('keydown-D', () => {
-            if (this.enemiesHeld > 0) {
-                this.enemiesHeld--;
-                this.physics.add.sprite(this.player.x + 16, this.player.y + 16, 'enemyRoped');
-                this.heldText.setText(`held: ${this.enemiesHeld}`);
+            if (this.enemiesHeld.length > 0) {
+                let currentEnemy = this.enemiesHeld.pop();
+                currentEnemy.setActive(true);
+                currentEnemy.setVisible(true);
+                currentEnemy.body.enable = true;
+                currentEnemy.setPosition(this.player.x + 16, this.player.y + 16);
+                this.heldText.setText(`held: ${this.enemiesHeld.length}`);
             }
         });
 
+        // enemies and players have collision, naturally
         this.physics.add.collider(this.enemies, this.player);
 
+        /* Here we check to see if the rope and enemy overlaps, if it does we deactivate the enemy and add it to the carried list. We also
+         * update its texture to prepare it to be dropped
+         */
         this.physics.add.overlap(this.rope, this.enemies, (rope, enemy) => {
-            if (this.enemiesHeld < this.maxEnemiesHeld) {
-                enemy.visible = false;
-                enemy.active = false;
-                enemy.body.enable = false;
-                this.enemiesHeld++;
-                this.heldText.setText(`held: ${this.enemiesHeld}`);
+            if (this.enemiesHeld.length < this.maxEnemiesHeld) {
+                enemy.visible           = false;
+                enemy.active            = false;
+                enemy.body.enable       = false;
+                enemy.data.values.roped = true;
+                enemy.setTexture('enemyRoped');
+                this.enemiesHeld.push(enemy);
+                this.heldText.setText(`held: ${this.enemiesHeld.length}`);
             }
         }, null, this);
 
+        //enemy movement
+        this.time.addEvent({ delay: 2000, callback: this.moveEnemies, callbackScope: this, loop: true });
 
-        
     }
 
     update() {
@@ -108,8 +130,11 @@ export class PlayGame extends Phaser.Scene {
         }
     }
 
-    checkGameOver() {
-
+    moveEnemies() {
+        this.enemies.children.iterate((child) => {
+            if (child.data.values.roped == false && child.data.values.dropped == false) {
+                child.setVelocity((Math.floor(Math.random() * 100)) * (Math.round(Math.random()) * 2 - 1), (Math.floor(Math.random() * 100)) * (Math.round(Math.random()) * 2 - 1));
+            }
+        })
     }
-
-};
+}
