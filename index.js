3b58ee8868842f687cbc7184dded8a8fd28c78b2
joeparrilla <joeparrilla@protonmail.com>
2019-07-28 21:45:30 -0400

    tring again

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-// var http = require('http');
+var express = require('express');
+var port = process.env.PORT || 9000;
+var app = express();
 
-// http.createServer(function (request, response) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World\n");
-// }).listen(process.env.PORT);
-
-var http = require('http');
-var fs = require('fs');
-
-const PORT=8080; 
-
-fs.readFile('index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+app.use(express.static(__dirname + '/dist'));
+app.listen(port, function(){
+	console.log('listening on ' + port);
 });
